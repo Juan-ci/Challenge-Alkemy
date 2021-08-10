bf51ede3804f4e982bf0e847ceaba6f2889c1a0f
Juan Ignacio Caballero <juan.ci.caballero@gmail.com>
2021-08-10 15:45:59 -0300

    CriteriaQuery(filtrado characters)

--- src/main/java/com/example/DisneyAPI/controllers/CharacterController.java
+++ src/main/java/com/example/DisneyAPI/controllers/CharacterController.java
@@ -2,7 +2,10 @@ package com.example.DisneyAPI.controllers;
 
 import com.example.DisneyAPI.dto.CharacterDto;
 import com.example.DisneyAPI.services.CharacterService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +24,30 @@ public class CharacterController {
     CharacterService characterService;
     
     @GetMapping()
+    public List<CharacterDto> getCharacter(
+    @RequestParam(value = "name",required = false) Optional<String> nombre,
+    @RequestParam(value = "edad",required = false) Optional<Long> edad,
+    @RequestParam(value = "idcharacter",required = false) Optional<Long> idCharacter
+    ){
+        Map< String, Object > filterBy = new HashMap<>();
+        
+        nombre.ifPresent( name -> filterBy.put("nombreCharacter", name));
+        edad.ifPresent( age -> filterBy.put("age", age));
+        idCharacter.ifPresent( idcharacter -> filterBy.put("idcharacter", idcharacter));
+        //System.out.println(filterBy); Todos los valores en el map
+        return characterService.getCharacters(filterBy);
+    }
+    
+    @GetMapping(path = "/{id}")
+    public CharacterDto getCharacterById(@PathVariable Long id){
+        return characterService.getCharacter(id);
+    }
+    /*
+    @GetMapping()
     public List<CharacterDto> getCharacter(){
         return characterService.getCharacters();
     }
+    */
     
     @PostMapping()
     public CharacterDto guardarPersonaje(@RequestBody CharacterDto character){
--- src/main/java/com/example/DisneyAPI/services/CharacterService.java
+++ src/main/java/com/example/DisneyAPI/services/CharacterService.java
@@ -8,13 +8,87 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.DisneyAPI.Repository.ICharacterRepository;
 import com.example.DisneyAPI.dto.CharacterDto;
 import java.util.List;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 @Service
 public class CharacterService {
-    
+
     @Autowired
     ICharacterRepository characterRepository;
-    
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Transactional(readOnly = true)
+    public List<CharacterDto> getCharacters(Map< String, Object> filterBy) {
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery criteriaQuery = criteriaBuilder.createQuery();
+        Root<CharacterModel> character = criteriaQuery.from(CharacterModel.class);
+
+        //Constructing list of parameters
+        List<Predicate> predicates;
+        predicates = new ArrayList();
+
+        if (filterBy.containsKey("nombreCharacter")) {
+            predicates.add(criteriaBuilder.equal(character.get("nombre"), filterBy.get("nombreCharacter")));
+
+        }
+
+        if (filterBy.containsKey("age")) {
+            predicates.add(criteriaBuilder.equal(character.get("edad"), filterBy.get("age")));
+        }
+
+        System.out.println("PREDICATES" + predicates);
+
+        criteriaQuery.select(character)
+                .where(predicates.toArray(new Predicate[]{}));
+
+        List<CharacterModel> characterBD2 = entityManager.createQuery(criteriaQuery).getResultList();
+
+        System.out.println("Resultado " + characterBD2);
+
+        List<CharacterModel> characterBD;
+        characterBD = entityManager.createQuery(criteriaQuery).getResultList();
+
+        List<CharacterDto> characterDto = new ArrayList();
+
+        characterBD.forEach(characterEnt -> {
+            characterDto.add(CharacterDto.getImagenNombre(characterEnt));
+        });
+        /*
+         *ENTENDER COMO USAR EXPRESION LAMBDA
+         */
+        return characterDto;
+    }
+
+    @Transactional(readOnly = true)
+    public CharacterDto getCharacter(Long idCharacter) {
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery criteriaQuery = criteriaBuilder.createQuery();
+        Root<CharacterModel> character = criteriaQuery.from(CharacterModel.class);
+
+        System.out.println("EL ID ES: " + idCharacter);
+        criteriaQuery.select(character)
+                .where(criteriaBuilder.equal(character.get("idCharacter"), idCharacter));
+
+        CharacterModel characterBD;
+        characterBD = (CharacterModel) entityManager.createQuery(criteriaQuery).getSingleResult();
+
+        CharacterDto characterDto;
+        characterDto = CharacterDto.getImagenNombre(characterBD);
+
+        return characterDto;
+    }
+
+    /*
     @Transactional(readOnly = true)
     public List<CharacterDto> getCharacters(){
         List<CharacterModel> characterBD = characterRepository.findAll();
@@ -24,12 +98,10 @@ public class CharacterService {
         for (CharacterModel characterEnt : characterBD) {
             characterDto.add(CharacterDto.getImagenNombre(characterEnt));
         }
-        /*
          *ENTENDER COMO USAR EXPRESION LAMBDA
-         */
         return characterDto;
     }
-    
+     */
     @Transactional
     public CharacterDto saveCharacter(CharacterDto character) {
         CharacterModel characterBD;
@@ -37,7 +109,7 @@ public class CharacterService {
         character.setIdCharacter(characterBD.getIdCharacter());
         return character;
     }
-    
+
     @Transactional
     public boolean deleteCharacter(Long id) {
         try {
@@ -47,7 +119,7 @@ public class CharacterService {
             return false;
         }
     }
-    
+
     /*
      * CREAR METODO PARA BUSQUEDA CON FILTRADO
      * CriteriaQuery
